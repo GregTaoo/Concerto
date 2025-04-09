74eb8a448c9861ee0e20255830f7c44a391bc7fb
GregTao <gregtaoo@outlook.com>
2025-04-09 18:13:03 +0800

    fix. 下载音频文件元数据修正

--- src/main/java/top/gregtao/concerto/player/MusicPlayerHandler.java
+++ src/main/java/top/gregtao/concerto/player/MusicPlayerHandler.java
@@ -334,6 +334,7 @@ public class MusicPlayerHandler {
                                     AudioFile audioFile = AudioFileIO.read(file);
                                     Tag tag = audioFile.getTagOrCreateAndSetDefault();
                                     tag.setField(FieldKey.TITLE, metaData.title());
+                                    tag.setField(FieldKey.ARTIST, metaData.author());
                                     tag.setField(FieldKey.ARTISTS, metaData.author());
                                     tag.setField(FieldKey.LYRICS, lyrics);
                                     if (!metaData.headPictureUrl().isEmpty()) {

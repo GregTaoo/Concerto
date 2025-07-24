60d8016499e59a65e1262e3ccd68857c48e9f84c
GregTao <gregtaoo@outlook.com>
2025-07-24 22:33:36 +0800

    fix: 移除导致部分mp3无法播放的jaadec；QQ音乐个人歌单未能加载

--- src/main/java/top/gregtao/concerto/screen/ConcertoScreen.java
+++ src/main/java/top/gregtao/concerto/screen/ConcertoScreen.java
@@ -46,7 +46,7 @@ public class ConcertoScreen extends Screen {
             );
         }
 
-        this.message = this.addDrawableChild(new NarratedMultilineTextWidget(
+        this.message = this.addSelectableChild(new NarratedMultilineTextWidget(
                 this.width, Text.empty(), this.textRenderer, 12));
         this.message.visible = false;
         this.initTabNavigation();
@@ -75,5 +75,6 @@ public class ConcertoScreen extends Screen {
         this.renderBackground(matrices, mouseX, mouseY, delta);
         super.render(matrices, mouseX, mouseY, delta);
         matrices.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 5, 0xffffffff);
+        this.message.render(matrices, mouseX, mouseY, delta);
     }
 }

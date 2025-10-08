15ee8c790fd01c4a8d6fd269f3871746b5a01595
GregTao <gregtaoo@outlook.com>
2025-10-08 20:41:52 +0800

    chore: code style

--- src/main/java/top/gregtao/concerto/ConcertoClient.java
+++ src/main/java/top/gregtao/concerto/ConcertoClient.java
@@ -77,18 +77,17 @@ public class ConcertoClient implements ClientModInitializer {
                     PresetPlaylistsConfig.LOCAL_PLAYLISTS.read();
 					NeteaseCloudApiClient.LOCAL_USER.updateLoginStatus();
 					QQMusicApiClient.LOCAL_USER.updateLoginStatus();
+
+					// 酷狗音乐相关
 					KuGouMusicApiClient.LOCAL_USER.updateLoginStatus();
 					// 刷新 token, 延长 token 有效时间
 					KuGouMusicApiClient.INSTANCE.refreshToken();
 					// 更新 VIP 状态
 					KuGouMusicApiClient.LOCAL_USER.updateVIPStatus();
 					// 自动获取每日酷狗音乐 VIP
-					ClientConfig.ClientConfigOptions options = ClientConfig.INSTANCE.options;
-					if (
-							options.kuGouMusicLite
-							&& KuGouMusicApiClient.LOCAL_USER.isLoggedIn()
-							&& options.autoGetKuGouDailyVIP
-					) {
+					if (ClientConfig.INSTANCE.options.kuGouMusicLite &&
+							KuGouMusicApiClient.LOCAL_USER.isLoggedIn() &&
+							ClientConfig.INSTANCE.options.autoGetKuGouDailyVIP) {
 						KuGouMusicApiClient.INSTANCE.receiveVip();
 					}
 				});

a403df9347b41c32cac6fde992988ea3bd8c1ca0
IMG <1973581872@qq.com>
2025-11-01 22:34:24 +0800

    fix: 修复一些 bug
    - 修复酷狗音乐无歌手信息时返回未知歌曲信息的 bug
    - 修复酷狗音乐领取每日 vip 时阻塞渲染线程的 bug
    - 修复 URLImageWidget 加载图片时阻塞渲染线程的 bug

--- src/main/java/top/gregtao/concerto/screen/widget/URLImageWidget.java
+++ src/main/java/top/gregtao/concerto/screen/widget/URLImageWidget.java
@@ -160,10 +160,13 @@ public class URLImageWidget implements Drawable, Widget, AutoCloseable {
     }
 
     private void uploadImage(BufferedImage image, Runnable callback) {
+        // ImageIO.write() 是耗时操作, 会卡住渲染线程
+        NativeImage nativeImage = toNativeImage(image);
         MinecraftClient.getInstance().submit(() -> {
-            if (this.texture != null)
-              MinecraftClient.getInstance().getTextureManager().destroyTexture(this.textureId);
-            this.texture = new NativeImageBackedTexture(this.textureId::toString, toNativeImage(image));
+            if (this.texture != null) {
+                MinecraftClient.getInstance().getTextureManager().destroyTexture(this.textureId);
+            }
+            this.texture = new NativeImageBackedTexture(this.textureId::toString, nativeImage);
             MinecraftClient.getInstance().getTextureManager().registerTexture(this.textureId, this.texture);
         }).thenRun(callback);
     }
